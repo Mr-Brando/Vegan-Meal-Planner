e50e55e93a7a5f318749738b521a02ca64c51e1b
Mr-Brando <brandon-215@hotmail.com>
2026-03-06 22:08:53 -0500

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -1,83 +1,87 @@
-let recipes=[
+let recipes = [
 
 {
-
-name:"Lentil Sweet Potato Stew",
-
+name:"Apple Cinnamon Oatmeal",
 servings:4,
-
 ingredients:[
-
-{name:"lentils cups",qty:2},
-{name:"sweet potatoes",qty:3},
-{name:"onions",qty:2},
-{name:"carrots",qty:3}
-
+{name:"rolled oats cups",qty:2},
+{name:"apples",qty:2},
+{name:"cinnamon tsp",qty:1},
+{name:"dates",qty:4}
 ],
-
 steps:[
-
-"Dice vegetables",
-"Cook onion and garlic",
-"Add vegetables",
-"Add lentils and broth",
-"Simmer 30 minutes"
-
+"Bring 4 cups water to boil",
+"Add oats and diced apples",
+"Cook 5 minutes",
+"Stir in cinnamon and chopped dates"
 ]
-
 },
 
 {
-
-name:"Chickpea Veggie Wraps",
-
+name:"Banana Oat Pancakes",
 servings:4,
-
 ingredients:[
+{name:"rolled oats cups",qty:2},
+{name:"bananas",qty:2},
+{name:"plant milk cups",qty:1.5}
+],
+steps:[
+"Blend all ingredients",
+"Pour batter on hot pan",
+"Cook 2 minutes each side"
+]
+},
 
+{
+name:"Chickpea Veggie Wraps",
+servings:4,
+ingredients:[
 {name:"chickpeas cups",qty:2},
 {name:"tortillas",qty:4},
 {name:"avocados",qty:2},
 {name:"lettuce heads",qty:1}
-
 ],
-
 steps:[
-
 "Mash chickpeas",
-"Add avocado",
-"Spread on tortillas",
-"Add lettuce",
-"Wrap and serve"
-
+"Add diced avocado",
+"Spread onto tortillas",
+"Add lettuce and wrap"
 ]
-
 },
 
 {
-
-name:"Sweet Potato Black Bean Chili",
-
+name:"Lentil Sweet Potato Stew",
 servings:4,
-
 ingredients:[
-
+{name:"lentils cups",qty:2},
 {name:"sweet potatoes",qty:3},
-{name:"black beans cups",qty:2},
-{name:"onions",qty:1},
-{name:"tomatoes cans",qty:1}
-
+{name:"onions",qty:2},
+{name:"carrots",qty:3},
+{name:"garlic cloves",qty:4}
 ],
-
 steps:[
-
 "Dice vegetables",
-"Cook onions",
-"Add beans and tomatoes",
-"Simmer 25 minutes"
-
+"Sauté onion and garlic",
+"Add vegetables and lentils",
+"Add broth and simmer 30 minutes"
 ]
+},
 
+{
+name:"Black Bean Chili",
+servings:4,
+ingredients:[
+{name:"black beans cups",qty:2},
+{name:"tomatoes cans",qty:1},
+{name:"onions",qty:1},
+{name:"bell peppers",qty:2}
+],
+steps:[
+"Cook onions",
+"Add peppers",
+"Add beans and tomatoes",
+"Simmer 20 minutes"
+]
 }
 
 ];
