b6d7b6bbbc67f473ab4567f722df1ce4ad0c0b0c
Mr-Brando <brandon-215@hotmail.com>
2026-03-06 22:09:12 -0500

    Update mealplan.js

--- mealplan.js
+++ mealplan.js
@@ -1,9 +1,11 @@
-let mealPlan=[
+let mealPlan = [
 
-{day:1,breakfast:"Oatmeal",lunch:"Chickpea Wraps",dinner:"Lentil Sweet Potato Stew"},
-
-{day:2,breakfast:"Banana Smoothie",lunch:"Quinoa Salad",dinner:"Black Bean Chili"},
-
-{day:3,breakfast:"Chia Pudding",lunch:"Hummus Veggie Sandwich",dinner:"Vegetable Stir Fry"}
+{day:1,breakfast:"Apple Cinnamon Oatmeal",lunch:"Chickpea Veggie Wraps",dinner:"Lentil Sweet Potato Stew"},
+{day:2,breakfast:"Banana Oat Pancakes",lunch:"Quinoa Veggie Bowl",dinner:"Black Bean Chili"},
+{day:3,breakfast:"Green Smoothie",lunch:"Hummus Veggie Sandwich",dinner:"Vegetable Stir Fry"},
+{day:4,breakfast:"Overnight Oats",lunch:"Chickpea Salad",dinner:"Sweet Potato Curry"},
+{day:5,breakfast:"Fruit Oatmeal",lunch:"Bean Burrito Bowl",dinner:"Lentil Soup"},
+{day:6,breakfast:"Banana Smoothie",lunch:"Quinoa Salad",dinner:"Vegetable Chili"},
+{day:7,breakfast:"Berry Oats",lunch:"Hummus Wrap",dinner:"Chickpea Curry"}
 
 ];
