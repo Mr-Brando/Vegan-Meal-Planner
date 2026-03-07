4eda9de8b2e07a73826ff4305a99f85b83a7c76c
Mr-Brando <brandon-215@hotmail.com>
2026-03-06 21:53:21 -0500

    Add files via upload

---
+++ Vegan Meal Planner/mealplan.js
@@ -0,0 +1,9 @@
+let mealPlan=[
+
+{day:1,breakfast:"Oatmeal",lunch:"Chickpea Wraps",dinner:"Lentil Sweet Potato Stew"},
+
+{day:2,breakfast:"Banana Smoothie",lunch:"Quinoa Salad",dinner:"Black Bean Chili"},
+
+{day:3,breakfast:"Chia Pudding",lunch:"Hummus Veggie Sandwich",dinner:"Vegetable Stir Fry"}
+
+];
